662ca9131a5890343506fc1cf5c7702190b1a008
Sergey Sochienko <59665883+fenq2@users.noreply.github.com>
2020-04-02 21:32:20 +0300

    Add files via upload

---
+++ static/js/main.js
@@ -0,0 +1,120 @@
+"use strict";
+
+var locationItem = function locationItem() {
+  var selectCurrent = document.querySelectorAll('.location__header'),
+      selectItem = document.querySelectorAll('.location__item');
+  selectCurrent.forEach(function (item) {
+    item.addEventListener('click', function (e) {
+      e.currentTarget.parentElement.classList.toggle('is-open');
+    });
+  });
+  selectItem.forEach(function (item) {
+    item.addEventListener('click', function (e) {
+      e.currentTarget.closest('.location').querySelector('.location__current').innerHTML = e.currentTarget.innerHTML;
+      e.currentTarget.closest('.location').classList.remove('is-open');
+    });
+  });
+};
+
+locationItem();
+
+var sandwich = function sandwich() {
+  var $hideElement = $('.sandwich__content');
+  $('.sandwich__open-btn').on('click', function () {
+    $hideElement.addClass('sandwich__content-opend');
+  });
+  $('.sandwich__close-btn').on('click', function () {
+    $hideElement.removeClass('sandwich__content-opend');
+  });
+};
+
+sandwich();
+var swiper = new Swiper('.banner', {
+  effect: 'fade',
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    renderBullet: function renderBullet(index, className) {
+      return '<span class="' + className + '">' + "0" + (index + 1) + '</span>';
+    }
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  }
+});
+var reviews = new Swiper('.reviews-container', {
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+  navigation: {
+    nextEl: '.reviews__next',
+    prevEl: '.reviews__prev'
+  },
+  slidesPerView: 1,
+  spaceBetween: 15,
+  breakpoints: {
+    // when window width is >= 320px
+    600: {
+      slidesPerView: 2,
+      spaceBetween: 15
+    },
+    960: {
+      slidesPerView: 3,
+      spaceBetween: 15
+    }
+  }
+});
+var license = new Swiper('.license-container', {
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+  navigation: {
+    nextEl: '.license__next',
+    prevEl: '.license__prev'
+  },
+  slidesPerView: 2,
+  spaceBetween: 15,
+  breakpoints: {
+    // when window width is >= 320px
+    400: {
+      slidesPerView: 3,
+      spaceBetween: 15
+    },
+    600: {
+      slidesPerView: 4,
+      spaceBetween: 15
+    },
+    960: {
+      slidesPerView: 6,
+      spaceBetween: 15
+    }
+  }
+});
+$('.services__content').isotope({
+  itemSelector: '.isotope-item',
+  layoutMode: 'fitRows'
+});
+$('.services-nav__item').click(function () {
+  $('.services-nav__item').removeClass('services-nav__item--active');
+  $(this).addClass('services-nav__item--active');
+  var selector = $(this).attr('data-filter');
+  $('.services__content').isotope({
+    filter: selector
+  });
+  return false;
+});
+
+var filter = function filter() {
+  var $hideElement = $('.services-filter__nav');
+  $('.services-filter__open').on('click', function () {
+    $hideElement.addClass('services-filter__nav-opend');
+  });
+  $('.services-nav__item').on('click', function () {
+    $hideElement.removeClass('services-filter__nav-opend');
+  });
+};
+
+filter();
